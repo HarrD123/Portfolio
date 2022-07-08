0913f3177d42c6b0e7d7cb0074ccfa95f7b883e1
Harry Dietrich <dietrih@fvfzwal2j1wk.gateway>
2022-07-08 13:17:55 +1000

    added some port stuff

--- port.js
+++ port.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+let harry = document.getElementById("harry");
+let programming = document.getElementById("programming");
+let javascript = document.getElementById("javascript");
+let htmlcss = document.getElementById("htmlcss");
+let frontend = document.getElementById("frontend");
+let backend = document.getElementById("backend");
+
+window.addlistenerEvent('scroll', function () {
+    let value = window.scrollY;
+    harry.style.left = value + "px";
+    programming.style.right = value * + 'px';
+})
